f422aacd147da5dfdeed9dbd8a1edd10910d0b9c
ArshadShahoriar <60341754+arshadShahoriar@users.noreply.github.com>
2020-10-25 13:20:59 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const http 	= 	require('http');
+const fs	=	require('fs');
+
+const server = http.createServer((request, response)=>{
+	
+	console.log(request.url);
+	console.log(request.method);
+	//response.write('<h1>Node Http Server works...</h1>');
+	//response.end();
+
+	if(request.url == '/'){
+		response.write("this is index page");
+		response.end();
+
+	}
+	else if( request.url == '/home'){
+		var content = fs.readFileSync('home.html');
+		response.write(content.toString());
+		response.end();
+	
+	}
+	else if( request.url == '/contract'){
+		var content = fs.readFileSync('contract.html');
+		response.write(content.toString());
+		response.end();
+	
+	}
+	else if( request.url == '/about'){
+		var content = fs.readFileSync('about.html');
+		response.write(content.toString());
+		response.end();
+	
+	}
+	else{
+		response.write("404 not found!");
+		response.end();
+	}
+});
+
+server.listen(3000);
+console.log('server started at 3000...');
